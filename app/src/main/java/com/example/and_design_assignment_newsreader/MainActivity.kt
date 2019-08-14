c1d64a218886248c8d4b836038551b2598b94a6c
corgipower <greensborodogtraining@gmail.com>
2019-08-14 16:48:46 -0400

    build intent button in main activity kt

--- app/src/main/java/com/example/and_design_assignment_newsreader/MainActivity.kt
+++ app/src/main/java/com/example/and_design_assignment_newsreader/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.and_design_assignment_newsreader
 
+import android.app.ListActivity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sign_in.setOnClickListener {
+            val intent = Intent(this, ListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
